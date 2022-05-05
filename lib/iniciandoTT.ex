b1eb04573ab3968f2afc0a619124bc902da130ac
andy_local <A01731650@tec.mx>
2022-05-05 14:37:38 -0500

    Automata a2

--- lib/iniciandoTT.ex
+++ lib/iniciandoTT.ex
@@ -18,3 +18,24 @@ defmodule IniciandoTT do
     }
   end
 end
+
+def a2 do
+  %{
+    alpha: ~w[a b],
+    states: [P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10],
+    istate: P0,
+    fstates: [P10],
+    delta: %{
+      {[P0], nil} => [P1, P7],
+      {[P1], nil} => [P2, P3],
+      {[P2], "a"} => [P4],
+      {[P3], "b"} => [P5],
+      {[P4], nil} => [P6],
+      {[P5], nil} => [P6],
+      {[P6], nil} => [P1, P7],
+      {[P7, "a"]} => [P8],
+      {[P8, "b"]} => [P9],
+      {[P9, "b"]} => [P10]
+    }
+  }
+end
