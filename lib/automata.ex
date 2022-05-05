fd9e50c1086dd42db30c41c8d2c5a0e3f71bc684
ale-O-local <alejo.ubeto@gmail.com>
2022-05-04 22:44:47 -0500

    Implementación de la estructura de funciones

--- lib/automata.ex
+++ lib/automata.ex
@@ -1,18 +1,11 @@
 defmodule Automata do
-  @moduledoc """
-  Documentation for `Automata`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Automata.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def determinize(n) do
+    #1: función determinize
+  end
+  def e_closure() do
+    #2: función e_closure
+  end
+  def e_determinize() do
+    #3: función e_determinize
   end
 end
